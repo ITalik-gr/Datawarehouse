540142c60c90cd4ffb52e7a6307b7572e60647a6
v.ITalik <vitalik.gricenko77@gmail.com>
2020-03-20 00:51:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  arrows: true,
+  centerMode: true,
+  canterPadding: '60px',
+  variableWidth: true,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+});
