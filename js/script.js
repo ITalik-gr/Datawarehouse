7c6b511d0f3be71976d27835b472b025f2d2c9f7
v.ITalik <vitalik.gricenko77@gmail.com>
2020-03-20 17:33:09 +0200

    ad modal window

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Slider
 $('.slider').slick({
   dots: true,
   infinite: false,
@@ -11,10 +11,36 @@ $('.slider').slick({
   prevArrow: $('.prev'),
   nextArrow: $('.next'),
 });
-
+//! Menu
 var trigger = document.getElementById('toggle');
 var box = document.getElementById('menu');
 
 toggle.addEventListener('click', function() {
   box.classList.toggle('active');
 });
+
+// ! Scroll
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1500);
+    return false;
+});
+
+// ! Modal
+
+let modal = document.getElementById('Modal');
+let btn = document.getElementById("modalBtn");
+let span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "flex";
+};
+span.onclick = function() {
+    modal.style.display = "none";
+};
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    };
+};
